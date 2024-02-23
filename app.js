639f372ceccd7f14c43685f248600cad6df2b1fa
Ahmed Alkooheji <ahmed@kooheji.info>
2024-02-23 20:28:23 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/pad/:number", (req, res) => {
+  const { number } = req.params;
+  const paddedNumber = number.toString().padStart(8, " ");
+
+  res.send(paddedNumber);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
